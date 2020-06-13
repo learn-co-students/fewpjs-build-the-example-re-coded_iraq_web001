d48112f732f2ab84366997d130dc052ce0431ccf
Qays Ayad <work.worker55@gmail.com>
2020-06-13 15:30:55 +0000

    Done.

--- main.js
+++ main.js
@@ -2,9 +2,37 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+
+
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded',function(e){
 
+  let hide = document.getElementById('modal')
+  hide.className = 'hidden'
+document.addEventListener('click', function(e){
+    if (e.target.classList.contains('like-glyph')){
+    mimicServerCall()
+    .then(()=>{
+      // e.target.innerText = e.target.innerText === EMPTY_HEART?FULL_HEART:EMPTY_HEART;
+      if ( e.target.innerText === FULL_HEART){
+      e.target.classList.remove('activated-heart');
+      e.target.innerText = EMPTY_HEART;
 
+    } else {
+      e.target.classList.add('activated-heart');
+      e.target.innerText = FULL_HEART;
+
+    }
+    })
+    .catch((err)=>{
+      hide.className = ''
+      hide.innerText = err
+      setTimeout(function(){ hide.className = 'hidden'; }, 5000);
+        })
+
+    }
+})
+})
 
 
 //------------------------------------------------------------------------------
